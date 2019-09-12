b068f4d1dd204ff1d61f49b89090ed90b6433b41
unknown <>
2019-09-12 10:34:49 +0800

    demo基础框架

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <input type="text" id="text">
+    <p id="show">123456</p>
+  </div>
+</template>
+<script type='text/ecmascript-6'>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  mounted() {}
+};
+</script>
+<style scoped>
+</style>
